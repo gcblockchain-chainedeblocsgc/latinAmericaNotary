b0879af2cb0117a15fcd3ac9c77269fea8c38b0d
nemesis1346 <marco.maigua1346@gmail.com>
2018-04-20 13:49:57 -0600

    react http attempts, following steps to implement ui components

--- reactFrontEnd/src/App.js
+++ reactFrontEnd/src/App.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      username: ''
+    }
+    this.handleClick = this.handleClickImplementation.bind(this);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.handleClick}>ClickMe</button>
+        <p>{this.state.username}</p>
       </div>
     );
+  }
+  handleClickImplementation() {
+    axios.get('https://api.github.com/users/maecapozzi')
+      .then(response => {
+        this.setState({ username: response.data.name });
+        console.log(response);
+      }
+      );
   }
 }
 
